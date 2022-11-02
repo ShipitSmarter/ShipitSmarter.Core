3762cbf9a62dadd25c385fcbac73996842b4c820
scott-david-walker <scott.david.walker@hotmail.com>
2022-11-02 10:37:00 +0100

    feat: Add exceptions

--- ShipitSmarter.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ShipitSmarter.Core
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/ShipitSmarter.Core/Exceptions/DomainException.cs
@@ -0,0 +1,13 @@
+﻿namespace ShipitSmarter.Core.Exceptions;
+
+public abstract class DomainException : Exception
+{
+    public int StatusCode { get; protected set; } = 500;
+    public string Title => Message;
+    public string? Detail { get; protected set; }
+    public Dictionary<string, object> Extensions { get; } = new();
+
+    protected DomainException(string message) : base(message)
+    {
+    }
+}
