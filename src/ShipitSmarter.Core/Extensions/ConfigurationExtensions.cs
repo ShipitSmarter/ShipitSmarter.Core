49aed4fdbf23ad58d2293ad95267c51ec992a0be
ThumNet <jeffreytummers@gmail.com>
2023-01-25 10:32:13 +0100

    feat: Require fewer dependencies

--- src/ShipitSmarter.Core/Extensions/ConfigurationExtensions.cs
+++ src/ShipitSmarter.Core/Extensions/ConfigurationExtensions.cs
@@ -2,23 +2,22 @@
 // ReSharper disable CheckNamespace
 namespace Microsoft.Extensions.Configuration;
 
-
 /// <summary>
 /// Set of extensions for Configuration Manager
 /// </summary>
-public static class ConfigurationManagerExtensions
+public static class ConfigurationExtensions
 {
     /// <summary>
     /// Get environment variable and throw exception if empty
     /// </summary>
-    /// <param name="configurationManager"></param>
+    /// <param name="configuration"></param>
     /// <param name="key">Environment variable</param>
     /// <typeparam name="T">Type of key expected</typeparam>
     /// <returns>Type of T</returns>
     /// <exception cref="ApplicationException">Key not defined</exception>
-    public static T GetValueOrThrow<T>(this ConfigurationManager configurationManager, string key)
+    public static T GetValueOrThrow<T>(this IConfiguration configuration, string key)
     {
-        return configurationManager.GetValue<T>(key) ??
+        return configuration.GetValue<T>(key) ??
                throw new ApplicationException($"'{key}' not defined in environment variables");
     }
 }
