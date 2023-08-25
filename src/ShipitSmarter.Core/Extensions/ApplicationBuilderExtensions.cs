9238bf949aabe25bbbf6791b22fff302a6f0c499
ThumNet <jeffreytummers@gmail.com>
2023-08-25 15:27:45 +0200

    feat: Add UseInvariantCulture as ApplicationBuilder Extension

---
+++ src/ShipitSmarter.Core/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,28 @@
+using System.Globalization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Localization;
+
+namespace ShipitSmarter.Core.Extensions;
+
+/// <summary>
+/// Extensions for <see cref="IApplicationBuilder"/>
+/// </summary>
+public static class ApplicationBuilderExtensions
+{
+    /// <summary>
+    /// Configure the <see cref="IApplicationBuilder"/> to use <see cref="CultureInfo.InvariantCulture"/> for Request Localization
+    /// </summary>
+    public static IApplicationBuilder UseInvariantCulture(this IApplicationBuilder app)
+    {
+        var supportedCultures = new[] { CultureInfo.InvariantCulture };
+        app.UseRequestLocalization(new RequestLocalizationOptions
+        {
+            DefaultRequestCulture = new RequestCulture(supportedCultures[0]),
+            // Formatting numbers, dates, etc.
+            SupportedCultures = supportedCultures,
+            // UI strings that we have localized.
+            SupportedUICultures = supportedCultures
+        });
+        return app;
+    }
+}
