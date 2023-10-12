b23d1aad343413368cad15ff07c46ddd7a50c3f6
bvanderhorn <bvanderhorn@gmail.com>
2023-10-12 15:30:38 +0200

    feat: add logic for local DateTime and absolute DateTimeOffset
    - Add custom JsonConverter to enforce time zone specification in DateTimeOffset
    - Add custom JsonConverter to prevent time zone specification in DateTime
    - Add custom Swagger schema filter to specify for each DateTime that time zone specification is not allowed
    - Add custom Swagger schema filter to specify for each DateTimeOffset that time zone specification is mandatory
    - Add SwashBuckle.SwaggerGen Nuget package
    - Add Microsoft.OpenAPI Nuget package

---
+++ src/ShipitSmarter.Core/Converters/MandatoryTimeZoneDateTimeOffsetConverter.cs
@@ -0,0 +1,42 @@
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ShipitSmarter.Core.Converters;
+
+/// <summary>
+/// Custom JsonConverter to enforce time zone specification for DateTimeOffset
+/// </summary>
+public class MandatoryTimeZoneDateTimeOffsetConverter : JsonConverter<DateTimeOffset>
+{
+    /// <summary>
+    /// Throws an exception if the DateTimeOffset has no time zone specification
+    /// </summary>
+    /// <param name="reader"></param>
+    /// <param name="typeToConvert"></param>
+    /// <param name="options"></param>
+    /// <returns></returns>
+    /// <exception cref="JsonException"></exception>
+    public override DateTimeOffset Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var dateTime = DateTime.Parse(reader.GetString()!, CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal);
+        if (dateTime.Kind != DateTimeKind.Utc)
+        {
+            throw new JsonException("Time zone specification for DateTimeOffset is mandatory");
+        }
+        var dateTimeOffset = DateTimeOffset.Parse(reader.GetString()!, CultureInfo.InvariantCulture);
+        
+        return dateTimeOffset;
+    }
+
+    /// <summary>
+    /// Default implementation
+    /// </summary>
+    /// <param name="writer"></param>
+    /// <param name="value"></param>
+    /// <param name="options"></param>
+    public override void Write(Utf8JsonWriter writer, DateTimeOffset value, JsonSerializerOptions options)
+    {
+        JsonSerializer.Serialize(writer, value, options);
+    }
+}
---
+++ src/ShipitSmarter.Core/Documentation/LocalDateTimeSchemaFilter.cs
@@ -0,0 +1,26 @@
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace ShipitSmarter.Core.Documentation;
+
+
+/// <summary>
+/// Swwagger filter to add a clarification to DateTime schema to indicate that it is not allowed to specify a time zone
+/// </summary>
+public class LocalDateTimeSchemaFilter : ISchemaFilter
+{
+    /// <summary>
+    /// Adds a clarification to DateTime schema to indicate that it is not allowed to specify a time zone
+    /// </summary>
+    /// <param name="schema"></param>
+    /// <param name="context"></param>
+    public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+    {
+        if (context.Type != typeof(DateTime) && context.Type != typeof(DateTime?))
+        {
+            return;
+        }
+
+        schema.Description += ". NOTE: Time zone specification is not allowed";
+    }
+}
