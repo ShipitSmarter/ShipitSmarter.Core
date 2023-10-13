7b60a514f0246d029901bebfa5ae8ddd2170217b
bvanderhorn <bvanderhorn@gmail.com>
2023-10-13 10:58:37 +0200

    test: add unit tests

---
+++ test/ShipitSmarter.Core.Tests/Converters/MandatoryTimeZoneDateTimeOffsetConverterTests.cs
@@ -0,0 +1,50 @@
+using System.Text.Json;
+using ShipitSmarter.Core.Converters;
+
+namespace ShipitSmarter.Core.Tests.Converters;
+
+public class MandatoryTimeZoneDateTimeOffsetConverterTests
+{
+    private readonly JsonSerializerOptions _options = new()
+    {
+        Converters = { new MandatoryTimeZoneDateTimeOffsetConverter() }
+    };
+
+    [Fact]
+    public void Read_WithDateTimeOffsetWithoutTimeZone_ThrowsJsonException()
+    {
+        var dateTimeString = "2021-09-01T00:00:00";
+        
+        var exception = Assert.Throws<JsonException>(() => JsonSerializer.Deserialize<TestClass>(Wrap(dateTimeString), _options));
+        Assert.Equal("Time zone specification for DateTimeOffset is mandatory", exception.Message);
+    }
+    
+    [Fact]
+    public void Read_WithDateTimeOffsetWithTimeZone_ReturnsDateTimeOffset()
+    {
+        var dateTimeString = "2021-09-01T00:00:00+02:00";
+        
+        var testClass = JsonSerializer.Deserialize<TestClass>(Wrap(dateTimeString), _options);
+        Assert.Equal(new DateTimeOffset(2021, 9, 1, 0, 0, 0, TimeSpan.FromHours(2)), testClass!.DateTimeOffset);
+        
+        // var backToString = JsonSerializer.Serialize(testClass, _options);
+        // Assert.Equal(Wrap(dateTimeString), backToString);
+    }
+    
+    [Fact]
+    public void Read_WithDateTimeOffsetWithUtcTimeZone_ReturnsDateTimeOffset()
+    {
+        var dateTimeString = "2021-09-01T00:00:00Z";
+        
+        var testClass = JsonSerializer.Deserialize<TestClass>(Wrap(dateTimeString), _options);
+        Assert.Equal(new DateTimeOffset(2021, 9, 1, 0, 0, 0, TimeSpan.Zero), testClass!.DateTimeOffset);
+    }
+
+    private string Wrap(string dateTimeString)
+    {
+        return @"{""DateTimeOffset"":""" + dateTimeString + @"""}";
+    }
+    
+    [Serializable]
+    private record TestClass(DateTimeOffset DateTimeOffset);
+}
---
+++ test/ShipitSmarter.Core.Tests/Converters/NoTimeZoneAllowedDateTimeConverterTests.cs
@@ -0,0 +1,41 @@
+using System.Text.Json;
+using ShipitSmarter.Core.Converters;
+
+namespace ShipitSmarter.Core.Tests.Converters;
+
+public class NoTimeZoneAllowedDateTimeConverterTests
+{
+    private readonly JsonSerializerOptions _options = new()
+    {
+        Converters = { new NoTimeZoneAllowedDateTimeConverter() }
+    };
+    
+    [Theory]
+    [InlineData("2021-09-01T00:00:00+02:00")]
+    [InlineData("2021-09-01T00:00:00Z")]
+    public void Read_WithDateTimeWithTimeZone_ThrowsJsonException(string dateTimeString)
+    {
+        var exception = Assert.Throws<JsonException>(() => JsonSerializer.Deserialize<TestClass>(Wrap(dateTimeString), _options));
+        Assert.Equal("Time zone specification for DateTime is not allowed", exception.Message);
+    }
+    
+    [Fact]
+    public void Read_WithDateTimeWithUtcTimeZone_ReturnsDateTime()
+    {
+        var dateTimeString = "2021-09-01T00:00:00";
+        
+        var testClass = JsonSerializer.Deserialize<TestClass>(Wrap(dateTimeString), _options);
+        Assert.Equal(new DateTime(2021, 9, 1, 0, 0, 0, DateTimeKind.Utc), testClass!.DateTime);
+        
+        // var backToString = JsonSerializer.Serialize(testClass, _options);
+        // Assert.Equal(Wrap(dateTimeString), backToString);
+    }
+    
+    private string Wrap(string dateTimeString)
+    {
+        return @"{""DateTime"":""" + dateTimeString + @"""}";
+    }
+    
+    [Serializable]
+    private record TestClass(DateTime DateTime);
+}
