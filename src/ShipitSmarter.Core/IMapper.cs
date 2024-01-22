b0a9cf0aba79635fd6260dd82d556aa03481a951
Lennart van Koot <lennartvkoot@gmail.com>
2024-01-22 16:11:18 +0100

    feat: Add `IMapper` and `IMapperAsync` (#30)

---
+++ src/ShipitSmarter.Core/IMapper.cs
@@ -0,0 +1,25 @@
+/// <summary>
+/// Maps an object from <typeparamref name="TSource"/> to <typeparamref name="TDest"/>
+/// </summary>
+/// <typeparam name="TSource">The type of the source object</typeparam>
+/// <typeparam name="TDest">The type of the destination object</typeparam>
+public interface IMapper<TSource, TDest>
+{
+    /// <summary>
+    /// Maps an object from <typeparamref name="TSource"/> to <typeparamref name="TDest"/>
+    /// </summary>
+    TDest From(TSource source);
+}
+
+/// <summary>
+/// Maps an object from <typeparamref name="TSource"/> to <typeparamref name="TDest"/>
+/// </summary>
+/// <typeparam name="TSource">The type of the source object</typeparam>
+/// <typeparam name="TDest">The type of the destination object</typeparam>
+public interface IMapperAsync<TSource, TDest>
+{
+    /// <summary>
+    /// Maps an object from <typeparamref name="TSource"/> to <typeparamref name="TDest"/>
+    /// </summary>
+    Task<TDest> From(TSource source);
+}
