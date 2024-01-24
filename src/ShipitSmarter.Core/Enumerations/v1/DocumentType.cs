74fe99fe37daff67e2db36fa0aadff38d0bc4cd5
Michael Stocks <michaelstocks1@gmail.com>
2024-01-24 10:51:27 +0100

    feat: added DocumentType to shared enums

---
+++ src/ShipitSmarter.Core/Enumerations/v1/DocumentType.cs
@@ -0,0 +1,44 @@
+namespace ShipitSmarter.Core.Enumerations.v1;
+
+/// <summary>
+/// Type of document
+/// </summary>
+public enum DocumentType
+{
+    /// <summary>
+    /// Packing List
+    /// </summary>
+    PackingList,
+    /// <summary>
+    /// Commercial Invoice
+    /// </summary>
+    CommercialInvoice,
+    /// <summary>
+    /// Proformal Invoice
+    /// </summary>
+    ProformaInvoice,
+    /// <summary>
+    /// Certificate of Origin
+    /// </summary>
+    CertificateOfOrigin,
+    /// <summary>
+    /// Export Declaration
+    /// </summary>
+    ExportDeclaration,
+    /// <summary>
+    /// Letter of Credit
+    /// </summary>
+    LetterOfCredit,
+    /// <summary>
+    /// CMR
+    /// </summary>
+    Cmr,
+    /// <summary>
+    /// Bill of Lading
+    /// </summary>
+    BillOfLading,
+    /// <summary>
+    /// Other
+    /// </summary>
+    Other
+}
