e66fd4a0ff52ee117d0899b3f9df462808ae9a9e
bvanderhorn <bvanderhorn@gmail.com>
2024-01-26 14:16:48 +0100

    feat: ups premier enums

---
+++ src/ShipitSmarter.Core/Enumerations/HandlingUnit/v1/UpsPremierCategory.cs
@@ -0,0 +1,13 @@
+namespace ShipitSmarter.Core.Enumerations.HandlingUnit.v1;
+
+/// <summary>
+/// Ups Premier service level associated with the handling unit
+/// </summary>
+public enum UpsPremierCategory
+{
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+    Silver,
+    Gold,
+    Platinum
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+}
