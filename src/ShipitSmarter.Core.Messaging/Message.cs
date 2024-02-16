d6f9a2f46c9070f41fa8d15e85956b12fbf0582a
Lennart van Koot <lennartvkoot@gmail.com>
2024-02-16 11:40:59 +0100

    feat: Setup core messaging

---
+++ src/ShipitSmarter.Core.Messaging/Message.cs
@@ -0,0 +1,34 @@
+﻿namespace ShipitSmarter.Core.Messaging;
+
+/// <summary>
+/// DTO for sending and receiving messages.
+/// </summary>
+public class Message
+{
+    /// <summary>
+    /// Unique identifier of this message.
+    /// </summary>
+    public Guid Id { get; set; }
+    /// <summary>
+    /// The subject of the message. Tells the receiver how to handle this message.
+    /// </summary>
+    public string Subject { get; }
+    /// <summary>
+    /// An id correlating this message to a process.
+    /// </summary>
+    public Guid CorrelationId { get; set; }
+    /// <summary>
+    /// Extra attributes for this message.
+    /// </summary>
+    public Dictionary<string, string> Attributes { get; } = [];
+    /// <summary>
+    /// The serialized message data.
+    /// </summary>
+    public byte[] Data { get; }
+
+    public Message(string subject, byte[] data)
+    {
+        Subject = subject;
+        Data = data;
+    }
+}
