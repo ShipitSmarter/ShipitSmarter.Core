955163dd789f4d27cd700506043f226d7569d3c0
Jeffrey <jeffreytummers@shipitsmarter.com>
2024-02-28 16:13:30 +0100

    feat: Add DebugPublisher

---
+++ src/ShipitSmarter.Core.Messaging.Publisher/DebugPublisher.cs
@@ -0,0 +1,24 @@
+using System.Text.Json;
+using Microsoft.Extensions.Logging;
+
+namespace ShipitSmarter.Core.Messaging.Publisher;
+
+/// <summary>
+/// <see cref="IPublisher"/> implementation when running in debug where there is no Google PubSub topic available
+/// </summary>
+public class DebugPublisher : IPublisher
+{
+    private readonly ILogger<DebugPublisher> _logger;
+
+    public DebugPublisher(ILogger<DebugPublisher> logger)
+    {
+        _logger = logger;
+
+    }
+
+    public Task Publish<TMessage>(TMessage message) where TMessage : IMessageContract<TMessage>
+    {
+        _logger.LogInformation($"Mimic publishing message {{message}}", JsonSerializer.Serialize(message));
+        return Task.CompletedTask;
+    }
+}
--- src/ShipitSmarter.Core.Messaging.Publisher/ServiceCollectionExtensions.cs
+++ src/ShipitSmarter.Core.Messaging.Publisher/ServiceCollectionExtensions.cs
@@ -2,6 +2,7 @@ using Google.Cloud.PubSub.V1;
 using ShipitSmarter.Core.Messaging;
 using ShipitSmarter.Core.Messaging.Publisher;
 
+// ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection;
 
 public static class ServiceCollectionExtensions
@@ -22,6 +23,19 @@ public static class ServiceCollectionExtensions
 
         services.AddPublisherClient(topicName);
         services.AddScoped<IPublisher,GooglePublisherClient>();
+
+        return services;
+    }
+    
+    /// <summary>
+    /// Registers a <see cref="DebugPublisher"/> as <see cref="IPublisher"/>.
+    /// </summary>
+    /// <returns>A reference to this instance after the operation has completed.</returns>
+    public static IServiceCollection AddDebugPublisherClient(
+        this IServiceCollection services
+    )
+    {
+        services.AddSingleton<IPublisher, DebugPublisher>();
 
         return services;
     }
