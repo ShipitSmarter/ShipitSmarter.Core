cecc9c64c26706e3f502561191c832f84adee815
bvanderhorn <bvanderhorn@gmail.com>
2024-02-28 11:07:30 +0100

    feat: ftp server related updates
    - ProblemDetailsException
    - FtpServerHost
    - String generation with Nanoid
    - HttpHeadersExtensions => AddCorrelationId

---
+++ src/ShipitSmarter.Core.AspNet/Extensions/HttpHeadersExtensions.cs
@@ -0,0 +1,19 @@
+using System.Net.Http.Headers;
+
+namespace ShipitSmarter.Core.AspNet.Extensions;
+
+/// <summary>
+/// Extensions for <see cref="HttpHeaders"/>
+/// </summary>
+public static class HttpHeadersExtensions
+{
+    /// <summary>
+    /// Add a CorrelationId to the <see cref="HttpHeaders"/> using the <see cref="ICorrelator"/>
+    /// </summary>
+    /// <param name="headers">The Http Headers to be updated</param>
+    /// <param name="correlator">The correlator containing the actual correlation id</param>
+    public static void AddCorrelationId(this HttpHeaders headers, ICorrelator correlator)
+    {
+        headers.Add("x-correlation-id", correlator.CurrentCorrelationId.ToString());
+    }
+}
