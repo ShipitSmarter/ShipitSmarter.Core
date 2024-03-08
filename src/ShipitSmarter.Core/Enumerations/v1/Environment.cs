0d564fa2cffbe264d5e3dd0c49c868d9d37133ca
Jeffrey <jeffreytummers@shipitsmarter.com>
2024-03-08 16:54:20 +0100

    feat: Add Environment and PresetType enums

---
+++ src/ShipitSmarter.Core/Enumerations/v1/Environment.cs
@@ -0,0 +1,18 @@
+namespace ShipitSmarter.Core.Enumerations.v1;
+
+/// <summary>
+/// Available environments
+/// </summary>
+[Serializable]
+public enum Environment
+{
+    /// <summary>
+    /// Playground (test)
+    /// </summary>
+    Playground,
+    
+    /// <summary>
+    /// Production
+    /// </summary>
+    Production
+}
