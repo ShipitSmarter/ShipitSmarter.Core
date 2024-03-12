80263812b133c5901896d2c4e772dc7a80bd8cea
Michael Stocks <michaelstocks1@gmail.com>
2024-03-12 10:20:26 +0100

    feat: renamed presetType enum to FlexibleConfigType

--- src/ShipitSmarter.Core/Enumerations/v1/FlexibleConfigType.cs
+++ src/ShipitSmarter.Core/Enumerations/v1/FlexibleConfigType.cs
@@ -3,15 +3,20 @@ namespace ShipitSmarter.Core.Enumerations.v1;
 /// <summary>
 /// Defines the type of preset. This is used during Json Deserialization to discriminate between the type
 /// </summary>
-public enum PresetType
+public enum FlexibleConfigType
 {
     /// <summary>
     /// Preset package type
     /// </summary>
-    PackageType,
+    Preset_PackageType,
     
     /// <summary>
     /// Time slot for pickup
     /// </summary>
-    TimeSlotPickup
+    Preset_TimeSlot,
+    
+    /// <summary>
+    /// Shipment create page settings
+    /// </summary>
+    Setting_ShipmentCreatePage
 }
