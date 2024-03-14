a4ba9bcbd9d42acde8b4d51a092fdf5ee6681a4e
Michael Stocks <michaelstocks1@gmail.com>
2024-03-14 11:30:56 +0100

    feat: makes all fields required

--- src/ShipitSmarter.Core/Models/v1/AddressAccount.cs
+++ src/ShipitSmarter.Core/Models/v1/AddressAccount.cs
@@ -5,9 +5,11 @@ namespace ShipitSmarter.Core.Models.v1;
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 public class AddressAccount
 {
-    public string? Label { get; set; }
+    [RequiredNotDefault]
+    public string? Label { get; set; } = Defaults.String;
     [RequiredNotDefault]
     public string CarrierReference { get; set; } = Defaults.String;
-    public string? AccountNumber { get; set; }
+    [RequiredNotDefault]
+    public string? AccountNumber { get; set; } = Defaults.String;
 }
 #pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
