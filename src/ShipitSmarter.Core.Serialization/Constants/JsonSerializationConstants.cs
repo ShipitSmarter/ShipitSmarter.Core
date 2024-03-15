3d77aa94ac10caece64f6e56971d6b8c21417d60
Jeffrey Tummers <jeffreytummers@shipitsmarter.com>
2024-03-15 10:49:50 +0100

    refactor: Cleanup Secret logic, this is moved to Configs now.  (#55)
    * refactor: Cleanup Secret logic, this is moved to Configs now. Just SecretValue stays in Sis.Core
    
    * chore: Cleanup unused using

--- src/ShipitSmarter.Core.Serialization/Constants/JsonSerializationConstants.cs
+++ src/ShipitSmarter.Core.Serialization/Constants/JsonSerializationConstants.cs
@@ -1,8 +1,6 @@
 using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Text.Json.Serialization.Metadata;
-using ShipitSmarter.Core.Secrets;
 
 namespace ShipitSmarter.Core.Serialization.Constants;
 
@@ -16,23 +14,5 @@ public class JsonSerializationConstants
     {
         Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
         Converters = { new JsonStringEnumConverter() },
-        TypeInfoResolver = new DefaultJsonTypeInfoResolver
-        {
-            Modifiers = { IgnoreSecrets }
-        }
     };
-    
-    private static void IgnoreSecrets(JsonTypeInfo typeInfo)
-    {
-        foreach (var propInfo in typeInfo.Properties)
-        {
-            if (typeof(ISecretValue).IsAssignableFrom(propInfo.PropertyType))
-            {
-                // Disable both serialization and deserialization 
-                // by unsetting getter and setter delegates
-                propInfo.Get = null;
-                propInfo.Set = null;
-            }
-        }
-    }
 }
